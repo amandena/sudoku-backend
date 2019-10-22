dd940f16fd7ab05fb955e49e3280c49fbd60902e
amandena <manda.panda91@yahoo.com>
2019-10-21 22:56:41 -0400

    set up game controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,35 @@
-class GamesController < ApplicationController
+class Api::V1::GamesController < ApplicationController
+  before_action :find_game, only: [:show, :destroy]
+
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game
+      render json: @game
+    else
+      render json: {error: 'Error starting game'}
+    end
+  end
+
+  def show
+    render json: @game
+  end
+
+  def destroy
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:numbers, :difficulty, :won)
+  end
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
 end
