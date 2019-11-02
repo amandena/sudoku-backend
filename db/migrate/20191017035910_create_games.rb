c086d770110e4f24256bbfcff73f811767513005
amandena <manda.panda91@yahoo.com>
2019-11-02 16:34:47 -0400

    added default value to won attribute in games table

--- db/migrate/20191017035910_create_games.rb
+++ db/migrate/20191017035910_create_games.rb
@@ -3,7 +3,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
     create_table :games do |t|
       t.string :numbers
       t.string :difficulty
-      t.boolean :won
+      t.boolean :won, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_035910) do
   create_table "games", force: :cascade do |t|
     t.string "numbers"
     t.string "difficulty"
-    t.boolean "won"
+    t.boolean "won", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
